96845768c5d50c20a97287a015f55df7f37c2f4f
dellarosamarco <dellarosamarco00@gmail.com>
2022-11-29 21:17:37 +0100

    update: supported symbols

--- src/exchanges/exchange.coins.ts
+++ src/exchanges/exchange.coins.ts
@@ -4,8 +4,41 @@ export namespace ExchangeCoins {
     export const USDT = 'USDT';
     export const XLM = 'XLM';
     export const DOGE = 'DOGE';
+    export const ADA = 'ADA';
+    export const BNB = 'BNB';
+    export const XRP = 'XRP';
+    export const MATIC = 'MATIC';
+    export const DOT = 'DOT';
+    export const LTC = 'LTC';
+    export const DAI = 'DAI';
     
-    export type coins = typeof BITCOIN | typeof ETH | typeof USDT | typeof XLM | typeof DOGE;
+    export type coins = 
+        typeof BITCOIN | 
+        typeof ETH | 
+        typeof USDT |
+        typeof XLM | 
+        typeof DOGE | 
+        typeof ADA |
+        typeof BNB |
+        typeof XRP |
+        typeof MATIC |
+        typeof DOT |
+        typeof LTC |
+        typeof DAI
+    ;
 
-    export const literalCoins: string[] = [BITCOIN, ETH, USDT, XLM, DOGE];
+    export const literalCoins: string[] = [
+        BITCOIN, 
+        ETH, 
+        USDT, 
+        XLM, 
+        DOGE, 
+        ADA,
+        BNB,
+        XRP,
+        MATIC,
+        DOT,
+        LTC,
+        DAI
+    ];
 }
